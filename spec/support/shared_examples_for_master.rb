8ffc17e7b9df950eef7c54ebaf5b639603d004da
Brett Porter <brett@maestrodev.com>
2013-03-28 14:30:19 -0700

    bump to release versions

--- spec/support/shared_examples_for_master.rb
+++ spec/support/shared_examples_for_master.rb
@@ -60,7 +60,7 @@ shared_examples 'maestro master' do
     end
 
     it 'should have the right version' do
-      should contain_wget__authfetch('fetch-maestro-rpm').with_source(/-4.12.1\.rpm$/)
+      should contain_wget__authfetch('fetch-maestro-rpm').with_source(/-4.13.*\.rpm$/)
     end
   end
 end
