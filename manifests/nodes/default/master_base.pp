44fc422595bb0e0f6b6f2fcba3e5daa7e2ad01df
Carlos Sanchez <csanchez@maestrodev.com>
2013-09-16 11:25:24 +0200

    Move default nodes to a new folder so newly defined nodes take precedence
    Add master_base missing node
    Don't overwrite node if it already exists

---
+++ manifests/nodes/default/master_base.pp
@@ -0,0 +1,15 @@
+# A Maestro Master node with
+#
+# required services:
+# * Maestro
+# * ActiveMQ
+
+node 'master_base' inherits 'parent' {
+
+  include maestro::repository
+  include maestro_nodes::repositories
+
+  # Maestro
+  include maestro_nodes::nginx::maestroservernginx
+
+}
