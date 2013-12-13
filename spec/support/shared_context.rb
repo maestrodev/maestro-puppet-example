b0cff78c5f84d3d77697915c8e43871054fd30df
Carlos Sanchez <csanchez@maestrodev.com>
2013-12-13 13:50:33 +0100

    Update to 4.20.0 and 2.3.0

--- spec/support/shared_context.rb
+++ spec/support/shared_context.rb
@@ -1,7 +1,7 @@
 shared_context :maestro do
 
-  let(:maestro_version) { '4.19.3' }
-  let(:agent_version) { '2.2.0' }
+  let(:maestro_version) { '4.20.0' }
+  let(:agent_version) { '2.3.0' }
 
   let(:repo) {{
     "url" => "https://repo.maestrodev.com/archiva/repository/all",
