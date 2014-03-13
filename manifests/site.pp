8425c2cb7769913dff77427d8c1278b5461a14b9
Carlos Sanchez <csanchez@maestrodev.com>
2014-03-13 12:13:43 +0100

    Set Default path for resources that don't set it: camptocamp/postfix, camptocamp/augeas,...

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,10 @@
 hiera_include(classes, [""])
 
+# Default path for resources that don't set it: camptocamp/postfix, camptocamp/augeas,...
+Exec {
+  path => ['/usr/local/sbin','/usr/local/bin','/sbin','/bin','/usr/sbin','/usr/bin'],
+}
+
 import '../modules/maestro_nodes/manifests/nodes/*.pp'
 import 'nodes/*.pp'
 import 'nodes/default/*.pp'
