d38a8506a80c8badf2639009dde37af3ac17dca8
Brett Porter <brett@maestrodev.com>
2014-04-21 13:00:23 +1000

    fix specs

--- spec/support/shared_context.rb
+++ spec/support/shared_context.rb
@@ -1,7 +1,7 @@
 shared_context :maestro do
 
-  let(:maestro_version) { '5.0.2' }
-  let(:agent_version) { '2.5.0' }
+  let(:maestro_version) { '5.1.1' }
+  let(:agent_version) { '2.6.0' }
 
   let(:repo) {{
     "url" => "https://repo.maestrodev.com/archiva/repository/all",
